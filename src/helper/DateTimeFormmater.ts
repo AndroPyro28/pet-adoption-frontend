bb5bec1dc812670fd1d0c9a135f24ef4568b2cd0
Andropyro28 <menandroeugenio1028@gmail.com>
2022-10-10 12:39:53 +0800

    'update'

--- src/helper/DateTimeFormmater.ts
+++ src/helper/DateTimeFormmater.ts
@@ -26,8 +26,8 @@ export const dateTimeLocalFormatter = (dateTimeLocal: string) => {
     // const dateTimeLocalTrimmed = dateTimeRemoveZ(dateTimeLocal);
     const date = new Date(dateTimeLocal).toISOString().slice(0, 10);
     
-    // const time = new Date(dateTimeLocal).toLocaleString('en-US', { timeZone:"Asia/Manila", hour: 'numeric', minute: 'numeric', hour12: true })
-     const time = new Date(dateTimeLocal).toLocaleString('en-US', { timeZone:"Asia/Manila"})
+    const time = new Date(dateTimeLocal).toLocaleString('en-US', { timeZone:"Asia/Manila", hour: 'numeric', minute: 'numeric', hour12: true })
+    //  const time = new Date(dateTimeLocal).toLocaleString('en-US', { timeZone:"Asia/Manila"})
     
     return {
         date, time
