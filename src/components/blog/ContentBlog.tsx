036f29f3033564d7161c90e0c55c2c95313fd451
Andropyro28 <menandroeugenio1028@gmail.com>
2022-10-18 20:21:22 +0800

    'update'

--- src/components/blog/ContentBlog.tsx
+++ src/components/blog/ContentBlog.tsx
@@ -3,20 +3,23 @@ import { domParser } from '../../helper/DomParser'
 import { Blog } from '../../models/Blog'
 import { ContentContainer, ContentImageContainer, ContentTextContainer } from './components'
 
-function ContentBlog({data}: {data: Blog}) {
+function ContentBlog({ data }: { data: Blog }) {
   const content = domParser(data.content)
   return (
     <ContentContainer>
-        <ContentImageContainer >
+      {
+        data.photos?.length > 0 && <ContentImageContainer >
           {
             data.photos.map((img) => <img src={img.imageUrl} key={img.id} />)
           }
         </ContentImageContainer>
-        <ContentTextContainer>
-            <h1>{data.title}</h1>
-            <p dangerouslySetInnerHTML={{__html: `${content.innerHTML}`}}></p>
-        </ContentTextContainer>
-        
+      }
+
+      <ContentTextContainer>
+        <h1>{data.title}</h1>
+        <p dangerouslySetInnerHTML={{ __html: `${content.innerHTML}` }}></p>
+      </ContentTextContainer>
+
     </ContentContainer>
   )
 }
